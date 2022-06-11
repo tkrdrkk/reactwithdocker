1bf4b4602ce74886d81fe6acb97b2800e451aab6
tokiorush <tokiorush@users.noreply.github.com>
2022-06-12 00:18:08 +0900

    chore: 未使用インポート削除

--- front/src/components/modals/globalMenuModal.tsx
+++ front/src/components/modals/globalMenuModal.tsx
@@ -1,13 +1,8 @@
 import { Box, Button, Modal, ModalProps, styled } from "@mui/material";
-import React from "react";
 import mountainPhoto from "images/simon-berger-twukN12EN7c-unsplash.jpg";
-import { red } from "@mui/material/colors";
 
 const StyledModal = styled(Modal)({
-  backgroundImage: `url(${mountainPhoto})`,
-  backgroundSize: "cover",
-  backgroundPosition: "center",
-  backgroundRepeat: "no-repeat",
+  // backgroundImage: `url(${mountainPhoto})`,
 });
 
 export const GlobalMenuModal = ({
@@ -29,6 +24,10 @@ export const GlobalMenuModal = ({
           boxShadow: 24,
           // border: "2px solid #000",
           p: 4,
+          backgroundImage: `url(${mountainPhoto})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
         }}
       >
         <p
